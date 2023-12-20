8f8250f3963db9724a2baec46b363c0253988479
TRIỆU TRẦN <trieutran989@gmail.com>
2023-12-20 14:13:17 +0700

    met

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 import moment from 'moment';
-// export const API_BASE_URL = 'https://ticketpro.deece.vn';
-export const API_BASE_URL = 'http://127.0.0.1:8000';
+export const API_BASE_URL = 'https://ticketpro.deece.vn';
+// gitexport const API_BASE_URL = 'http://127.0.0.1:8000';
 
 
 export function formatDate(dateString) {
